0a2090b9236a12a39b724697923c02605a394b82
arsen1tn <arsen.tadevosian749@gmail.com>
2026-07-28 12:16:09 +0200

    A.X.O.N. v0.0.1 logger

---
+++ axon/core/logger.py
@@ -0,0 +1,13 @@
+import logging
+from pathlib import Path
+
+logger = logging.getLogger("A.X.O.N")
+logger.setLevel(logging.INFO)
+
+LOGS_DIR = Path("logs")
+LOGS_DIR.mkdir(exist_ok=True)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+logger.addHandler(console_handler)
+logger.info("Logger initialized successfully.")
